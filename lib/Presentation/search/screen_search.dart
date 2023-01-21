68037186b37b43efd43db8327b9f816c6927c72f
Irshad <irshadkhaleel.p@gmail.com>
2023-01-21 11:52:36 +0530

    search idle

--- lib/Presentation/search/screen_search.dart
+++ lib/Presentation/search/screen_search.dart
@@ -1,13 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_cpy/Core/constants.dart';
+import 'package:netflix_cpy/Presentation/search/widgets/search_idle.dart';
 
 class ScreenSearch extends StatelessWidget {
   const ScreenSearch({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CupertinoSearchTextField(
+                backgroundColor: Colors.grey.withOpacity(0.5),
+                prefixIcon: const Icon(
+                  CupertinoIcons.search,
+                  color: Colors.grey,
+                ),
+                suffixIcon: const Icon(
+                  CupertinoIcons.xmark_circle_fill,
+                  color: Colors.grey,
+                ),
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              kHeight,
+              Expanded(child: const SearchIdleWidget()),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/Presentation/search/widgets/search_idle.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:netflix_cpy/Core/colors.dart';
+import 'package:netflix_cpy/Core/constants.dart';
+
+const imageUrl =
+    'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/r9PkFnRUIthgBp2JZZzD380MWZy.jpg';
+
+class SearchIdleWidget extends StatelessWidget {
+  const SearchIdleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Top Searches',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        kHeight,
+        Expanded(
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemBuilder: (context, index) => TopSearchItemTile(),
+            separatorBuilder: (context, index) => kHeight20,
+            itemCount: 10,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TopSearchItemTile extends StatelessWidget {
+  const TopSearchItemTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Row(
+      children: [
+        Container(
+          width: screenWidth * 0.3,
+          height: 70,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(imageUrl),
+            ),
+          ),
+        ),
+        kWidth,
+        const Expanded(
+          child: Text(
+            'Movie Name',
+            style: TextStyle(
+              color: kWhiteColor,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const CircleAvatar(
+          backgroundColor: kWhiteColor,
+          radius: 20,
+          child: CircleAvatar(
+            backgroundColor: kBlackColor,
+            radius: 18,
+            child: Center(
+              child: Icon(
+                CupertinoIcons.play_fill,
+                color: kWhiteColor,
+                size: 18,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
